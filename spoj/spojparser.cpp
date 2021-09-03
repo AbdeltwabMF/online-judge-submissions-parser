751b4e6f698f7f46032a5fdc468b5cba7e00e99d
AbdeltwabMF <abdeltwab.m.fakhry@gmail.com>
2021-09-03 22:41:38 +0200

    Update sol link

--- spoj/spojparser.cpp
+++ spoj/spojparser.cpp
@@ -9,11 +9,6 @@
 
 using namespace std;
 
-void file() {
-  freopen("", "r", stdin);
-  freopen("", "w", stdout);
-}
-
 void fast() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -23,8 +18,9 @@ string submission_id, problem_name, lang, verdict;
 vector <vector<string>> submissions(0, vector<string>(4));
 vector <vector<string>> unique_subs(0, vector<string>(4));
 
-int main(int argc, char** argv) {
+int main() {
   fast();
+
   while(cin >> submission_id >> problem_name >> verdict >> lang) {
     if(verdict == "AC" || verdict == "100")
       submissions.push_back({submission_id, problem_name, verdict, lang});
@@ -41,7 +37,7 @@ int main(int argc, char** argv) {
   });
 
   unique_subs.emplace_back(submissions[0]);
-  for(int i = 1; i < submissions.size(); ++i) {
+  for(int i = 1; i < (int)submissions.size(); ++i) {
     if(submissions[i][1] != unique_subs.back()[1]) {
       unique_subs.emplace_back(submissions[i]);
     }
@@ -49,15 +45,18 @@ int main(int argc, char** argv) {
 
   sort(unique_subs.rbegin(), unique_subs.rend());
 
+  string sol_path = "spoj/solutions/";
   string root_url = "https://www.spoj.com/problems/";
   cout << "| # | Submission ID | Problem Name | Verdict | lang |" << endl;
   cout << "|:-:| :-----------: | :----------: | :-----: | :--: |" << endl;
 
-  for(int i = 0, c = unique_subs.size(); i < unique_subs.size(); ++i) {
+  for(int i = 0, c = (int)unique_subs.size(); i < (int)unique_subs.size(); ++i) {
     cout << "| " << c--;
-    for(int j = 0; j < unique_subs[i].size(); ++j) {
+    for(int j = 0; j < (int)unique_subs[i].size(); ++j) {
       if(j == 1)
         cout << " | [" << unique_subs[i][j] << "](" << root_url + unique_subs[i][j] << ")";
+      else if(j == 0)
+        cout << " | [" << unique_subs[i][j] << "](" << sol_path + unique_subs[i][1] << ")";
       else
         cout << " | " << unique_subs[i][j];
     }
